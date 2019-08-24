75ce2c3d37031974d98f64b854a0361f89a4de45
zxv <zxv@zxv.com>
2019-08-23 22:15:05 -0400

    misc

--- ssh.sh
+++ ssh.sh
@@ -3,14 +3,16 @@ apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
 
 local password=$(head /dev/urandom | tr -dc A-Za-z | head -c 20; echo "")
 local alias=$(head /dev/urandom | tr -dc A-Za-z | head -c 8; echo "")
+echo ${password}
 
-echo root:${password} | chpasswd
+echo "root:${password}" | chpasswd
 mkdir -p /var/run/sshd
 
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc && echo "export LD_LIBRARY_PATH" >> /root/.bashrc
-/usr/sbin/sshd -D &
-ssh -o "StrictHostKeyChecking no" -R $alias:22:localhost:22 serveo.net &
+
+/usr/sbin/sshd -D
+#ssh -o "StrictHostKeyChecking no" -R $alias:22:localhost:22 serveo.net &
 
 echo "Use the following command to connect to the SSH server:"
 echo "sshpass -p ${password} ssh -o "StrictHostKeyChecking no" -J serveo.net root@${alias}"
