e18cef95b2e07d773360f89ba384d256860ca17c
zxv <zxv@zxv.com>
2019-08-23 20:30:55 -0400

    Attempt to fix path issues

--- activate.sh
+++ activate.sh
@@ -1,5 +1,13 @@
-git clone https://github.com/rowanz/grover.git
-cd grover
+CONTENT_PATH=/content
+GPTME_PATH="${CONTENT_PATH}/gptme"
+
+# Install dependencies
 python3 -m pip install regex jsonlines
 python3 -m pip install -U tqdm
-python3 bootstrap.py
+
+# Set up grover
+git clone https://github.com/rowanz/grover.git
+cd grover
+
+# Begin bootstrap process
+python3 ${GPTME_PATH}/bootstrap.py
